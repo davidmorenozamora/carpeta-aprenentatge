7cf9c3959718b14d2f42c8be7b025fea0462b218
David Moreno <morenodtona@gmail.com>
2023-11-25 13:58:45 +0100

    Arregalado fetching de imagenes

--- src/app/ui/CategoryImage.tsx
+++ src/app/ui/CategoryImage.tsx
@@ -2,13 +2,23 @@ import {Image, Title} from '@mantine/core';
 import { Category } from '../lib/definitions';
 import css from './categoryImage.module.css';
 import Link from 'next/link';
+import NextImage from 'next/image';
 
 export default function CategoryImage({category}: {category: Category}) {
+    
+    let image;
+
+    try {
+        image = require(`@/app/ui/assets/categories/${category.image}`);
+    } catch (error) {
+        console.error(error);
+    }
+
     return (
         <Link href={`/categories/${category.id}`}>
             <div className={`${css.imageContainer}`}>
                 <div>
-                    <Image src={`../assets/categories/${category.image}`} className={`${css.image}`} alt={category.title}/>
+                    <Image component={NextImage} src={image} className={`${css.image}`} alt={category.title}/>
                 </div>
                 <Title order={4}>{category.title}</Title>
             </div>
